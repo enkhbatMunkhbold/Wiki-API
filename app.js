f5f8f7879351c9eadecb5a6ac584dc4e48e91160
unknown <e_munkhbold@yahoo.com>
2021-10-09 13:20:35 -0700

    Setup MongoDB, collection and document with 2 fields

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost: 27017/wikiDB", {
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -21,6 +21,32 @@ const articleSchema = {
 };
 
 const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function(req, res) {
+  Article.find(function(err, foundArticles) {
+    if(!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", function(req, res) {
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err) {
+    if(!err) {
+      res.send("Successfully added a new article!");
+    } else {
+      res.send(err)
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
