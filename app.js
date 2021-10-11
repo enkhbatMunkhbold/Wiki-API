720d14768bbe1c3dd16eba341ab77a2fe41ad8f0
unknown <e_munkhbold@yahoo.com>
2021-10-11 10:36:56 -0700

    Update code with chained route handlers using express

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res) {
+app.route("/articles")
+.get(function(req, res) {
   Article.find(function(err, foundArticles) {
     if(!err) {
       res.send(foundArticles);
@@ -30,9 +31,8 @@ app.get("/articles", function(req, res) {
       res.send(err);
     }
   });
-});
-
-app.post("/articles", function(req, res) {
+})
+.post(function(req, res) {
 
   const newArticle = new Article({
     title: req.body.title,
@@ -46,9 +46,8 @@ app.post("/articles", function(req, res) {
       res.send(err);
     }
   });
-});
-
-app.delete("/articles", function(req, res) {
+})
+.delete(function(req, res) {
   Article.deleteMany(function(err) {
     if(!err) {
       res.send("Successfully deleted all articles.");
